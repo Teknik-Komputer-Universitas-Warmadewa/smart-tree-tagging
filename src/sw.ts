80096b8af68773f986d51d3bdbde7442979d7e82
suryakumara <suryakumara33@gmail.com>
2025-02-22 07:13:08 +0800

    image pwa

--- src/sw.ts
+++ src/sw.ts
@@ -1,4 +1,6 @@
 import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
+import { Route, registerRoute } from "workbox-routing";
+import { CacheFirst } from "workbox-strategies";
 
 declare let self: ServiceWorkerGlobalScope;
 
@@ -7,3 +9,14 @@ cleanupOutdatedCaches();
 precacheAndRoute(self.__WB_MANIFEST);
 
 self.skipWaiting();
+
+// cache images
+const imageRoute = new Route(
+  ({ request, sameOrigin }) => {
+    return sameOrigin && request.destination === "image";
+  },
+  new CacheFirst({
+    cacheName: "images",
+  })
+);
+registerRoute(imageRoute);
