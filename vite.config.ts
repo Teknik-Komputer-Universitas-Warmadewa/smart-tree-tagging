f5be28d3f65d43e7872d7bc149790544366ddf6d
suryakumara <suryakumara33@gmail.com>
2025-04-28 12:13:51 +0800

    fix: maximum limit

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
       registerType: "autoUpdate",
       injectManifest: {
         swDest: "dist/sw.js",
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
       },
       manifest: {
         name: "Smart Tagging",
